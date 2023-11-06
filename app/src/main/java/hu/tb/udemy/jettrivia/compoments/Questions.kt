3b15c31750f22be4197aa7436c1b80c81ecfdada
BTheofil <btheofil7@gmail.com>
2023-11-06 15:14:33 +0100

    add dots

--- app/src/main/java/hu/tb/udemy/jettrivia/compoments/Questions.kt
+++ app/src/main/java/hu/tb/udemy/jettrivia/compoments/Questions.kt
@@ -1,9 +1,12 @@
 package hu.tb.udemy.jettrivia.compoments
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
@@ -11,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -39,6 +44,8 @@ fun Questions(viewModel: QuestionViewModel) {
 
 @Composable
 fun QuestionDisplay() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+
     Surface(
         modifier = Modifier
             .fillMaxSize()
@@ -51,6 +58,7 @@ fun QuestionDisplay() {
             horizontalAlignment = Alignment.Start
         ) {
             QuestionTracker(10, 100)
+            DrawDottedLine(pathEffect)
         }
     }
 }
@@ -59,6 +67,23 @@ fun QuestionDisplay() {
 @Composable
 fun QuestionDisplayPreview() {
     QuestionDisplay()
+}
+
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp),
+        onDraw = {
+            drawLine(
+                color = AppColors.mLightGray,
+                start = Offset(0f, 0f),
+                end = Offset(size.width, 0f),
+                pathEffect = pathEffect
+            )
+        }
+    )
 }
 
 @Composable
